86078827e731457dd7755c5a1416191935bd78c6
Jada Huang <67606418+huangjada@users.noreply.github.com>
2021-07-20 14:47:08 -0700

    calc can perform operation using previous result
    changed result into a global variable so that the program can "remember" previous result to use in new mathematical operation.

--- calculator-1.py
+++ calculator-1.py
@@ -1,22 +1,33 @@
 import numpy
 
+global result
+result = 0
+
 def add(x, y):
-    result = x + y
+    global result
+
+    result = float(x) + float(y)
     result = numpy.format_float_positional(result, trim='-')    # removes unnecessary trailing zeroes or decimal points from result
     return result
 
 def subtract(x, y):
-    result = x - y
+    global result
+
+    result = float(x) - float(y)
     result = numpy.format_float_positional(result, trim='-')
     return result
 
 def multiply(x, y):
-    result = x * y
+    global result
+
+    result = float(x) * float(y)
     result = numpy.format_float_positional(result, trim='-')
     return result
 
 def divide(x, y):
-    result = x / y
+    global result
+
+    result = float(x) / float(y)
     result = numpy.format_float_positional(result, trim='-')
     return result
 
@@ -28,25 +39,62 @@ while True:
     operation = input("Which operation would you like to choose: ")
 
     if operation == "add":
-        print("The answer is: ", add(x, y))
+        result = add(x, y)
+        print("The answer is: ", result, '\n')
 
     elif operation == "subtract":
-        print("The answer is: ", subtract(x, y))
+        result = subtract(x, y)
+        print("The answer is: ", result, '\n')
 
     elif operation == "multiply":
-        print("The answer is: ", multiply(x, y))
+        result = multiply(x, y)
+        print("The answer is: ", result, '\n')
 
     elif operation == "divide":
-        print("The answer is: ", divide(x, y))
+        result = divide(x, y)
+        print("The answer is: ", result, '\n')
 
     else:
-        print("Invalid input.")
+        print("Invalid input.", '\n')
 
-    # cont asks user if they want to keep using calculator
-    # if yes, the loop will start from the beginning again
+
+    # cont asks user if they want to perform additional operation using result and new input number
+    # if yes, user will be asked to input new number and the second while loop will begin
     # otherwise, loop will break
-    cont = input("Calculate new numbers? Y/N: ")
+    while True:
+        cont = input("Perform new calculation with current result? Y/N: ")
 
-    if cont == "N" or cont == "n":
-        print("Goodbye.")
+        if cont == "y" or cont == "Y":
+            x = result
+            result = 0
+            print("First number is: ", x)
+            y = float(input("Input second number: "))
+
+            print("Operations: add, subtract, multiply, or divide.")
+            operation = input("Which operation would you like to choose: ")
+
+            if operation == "add":
+                print("The answer is: ", add(x, y), '\n')
+
+            elif operation == "subtract":
+                print("The answer is: ", subtract(x, y), '\n')
+
+            elif operation == "multiply":
+                print("The answer is: ", multiply(x, y), '\n')
+
+            elif operation == "divide":
+                print("The answer is: ", divide(x, y), '\n')
+
+            else:
+                print("Invalid input.", '\n')
+
+        else:
+            break
+
+
+    # Restart asks user if they want to continue use of calculator with new numbers
+    restart = input("Restart calculation? Y/N: ")
+
+    if restart == "n" or restart == "N":
+        print("Goodbye.\n")
         break
